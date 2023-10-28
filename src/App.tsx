62433a28f6e5519a5411a45c74d917737028aaf6
Yorane Abbou <yorane.a@gmail.com>
2023-10-28 12:29:22 +0200

    Create Alert component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-    let cities = ["New York", "San Francisco", "Tokyo", "Paris"];
-
-    const onSelectedItem = (item: string) => console.log(item);
-
     return (
         <div>
-            <ListGroup
-                header={"Cities"}
-                items={cities}
-                onSelectedItem={onSelectedItem}
-            />
+            <Alert>
+                Hey <b>Listen</b> !
+            </Alert>
         </div>
     );
 }
