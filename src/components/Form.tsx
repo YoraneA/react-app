73eb85cb323e08df0051ae5c2770ada09e98ad66
Yorane Abbou <yorane.a@gmail.com>
2025-03-09 11:00:30 +0200

    Add useRef to the form field

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,19 +1,29 @@
-export default function Form() {
+import React, {useRef} from "react";
 
-  const handleSubmit  = (event: React.FormEvent<HTMLFormElement>) => {
+export default function Form() {
+  const nameRef = useRef(null);
+  const ageRef = useRef(null);
+  const person = {
+    name: '',
+    age: 0,
+  }
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log('Submitted');
+    person.name = nameRef.current.value;
+    person.age = ageRef.current.value;
+
+    console.log(person)
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">Name</label>
-        <input id="name" type="text" className="form-control"/>
+        <input ref={nameRef} id="name" type="text" className="form-control"/>
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">Age</label>
-        <input id="age" type="number" className="form-control"/>
+        <input ref={ageRef} id="age" type="number" className="form-control"/>
       </div>
       <button type="submit" className="btn btn-primary">Submit</button>
     </form>
