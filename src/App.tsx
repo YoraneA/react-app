1aadee4700a249027e833dc725fae6553f3b2065
Yorane Abbou <yorane.a@gmail.com>
2025-03-09 10:42:18 +0200

    Creating Form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import ExpandableText from "./components/ExpandableText.tsx";
+import Form from "./components/Form.tsx";
 
 export default function App() {
   return (
-    <ExpandableText maxLength={10}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nunc libero, vestibulum ut tincidunt vel, viverra at nulla. In fermentum orci id nunc pharetra viverra. Suspendisse at velit nec lectus eleifend gravida et vitae nisl. Sed sollicitudin porta luctus. Vestibulum nec condimentum ex, at sagittis sem. Cras sed elementum arcu. Duis efficitur sagittis risus, eu rutrum sapien scelerisque vel. Integer non risus at orci pretium tincidunt eu non orci. Donec lobortis erat vel efficitur hendrerit. Donec nec sem pretium, tempus libero non, tincidunt erat. Quisque pulvinar, purus vitae efficitur molestie, libero ex tempus ex, eget ultrices felis erat sit amet massa. Fusce arcu dolor, eleifend id nisl quis, consequat luctus nulla.
-    </ExpandableText>
+    <>
+      <Form/>
+    </>
   );
 }
