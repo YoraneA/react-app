affe776c5ea61e2f520925ab6b84cbb501babd1d
Yorane Abbou <yorane.a@gmail.com>
2025-03-10 10:21:18 +0200

    Control form by State Hook

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,29 +1,37 @@
-import React, {useRef} from "react";
+import React, {useState} from "react";
 
 export default function Form() {
-  const nameRef = useRef(null);
-  const ageRef = useRef(null);
-  const person = {
+  const [person, setPerson] = useState({
     name: '',
-    age: 0,
-  }
+    age: '',
+  })
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    person.name = nameRef.current.value;
-    person.age = ageRef.current.value;
-
-    console.log(person)
+    console.log(person);
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">Name</label>
-        <input ref={nameRef} id="name" type="text" className="form-control"/>
+        <input
+          onChange={(event) => setPerson({...person, name: event.target.value})}
+          value={person.name}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">Age</label>
-        <input ref={ageRef} id="age" type="number" className="form-control"/>
+        <input
+          onChange={(event) => setPerson({...person, age: parseInt(event.target.value)})}
+          value={person.age}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button type="submit" className="btn btn-primary">Submit</button>
     </form>
