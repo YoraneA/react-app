45bc91413adb59aedb2de4ab1d9285f2309d7457
Yorane Abbou <yorane.a@gmail.com>
2025-03-10 11:48:35 +0200

    Using Form validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,33 +1,29 @@
 import React, {useState} from "react";
+import {FieldValue, FieldValues, useForm} from "react-hook-form";
 
 export default function Form() {
-  const [person, setPerson] = useState({
-    name: '',
-    age: '',
-  })
+  const {register, handleSubmit, formState: {errors}} = useForm<FormData>();
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(person);
-  }
+
+  const onSubmit = (data: FieldValues) => console.log(data)
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">Name</label>
         <input
-          onChange={(event) => setPerson({...person, name: event.target.value})}
-          value={person.name}
+          { ...register('name', {required: true, minLength: 3}) }
           id="name"
           type="text"
           className="form-control"
         />
+        { errors.name?.type === 'required' && <p className="text-danger">The name field is required.</p> }
+        { errors.name?.type === 'minLength' && <p className="text-danger">The name must be at least 3 characters.</p> }
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">Age</label>
         <input
-          onChange={(event) => setPerson({...person, age: parseInt(event.target.value)})}
-          value={person.age}
+          { ...register('age') }
           id="age"
           type="number"
           className="form-control"
@@ -36,4 +32,9 @@ export default function Form() {
       <button type="submit" className="btn btn-primary">Submit</button>
     </form>
   )
+}
+
+interface FormData {
+  name: string;
+  age: string;
 }
