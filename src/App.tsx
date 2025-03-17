2f5609902f57226a52f4f5b32e863ee2b937adfd
Yorane Abbou <yorane.a@gmail.com>
2025-03-17 10:34:37 +0200

    Discovering useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,21 @@
-import Form from "./components/Form.tsx";
-import {useState} from "react";
-import ExpenseList from "./components/expense-tracker/components/ExpenseList.tsx";
-import ExpenseFilter from "./components/expense-tracker/components/ExpenseFilter.tsx";
-import ExpenseForm from "./components/expense-tracker/components/ExpenseForm.tsx";
+import {useEffect, useRef} from "react";
 
 export default function App() {
-  const [expenses, setExpenses] = useState<Expense[]>([]);
-  const [selectedCategory, setSelectedCategory] = useState<string>('');
+  const ref = useRef<HTMLInputElement>(null);
 
-  const expensesFiltered = selectedCategory
-    ? expenses.filter(e => e.category === selectedCategory)
-    : expenses;
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.focus();
+    }
+  })
 
-  const handleSubmit = (data: Expense) => {
-    const expense = { ...data, id: expenses.length + 1 };
-
-    setExpenses([...expenses, expense]);
-  }
-
-  const filterExpenses = (category: string) => {
-    setSelectedCategory(category);
-  }
-
-  const deleteExepense = (id: number) => {
-    setExpenses(expenses.filter(e => e.id !== id));
-  }
+  useEffect(() => {
+    document.title = "My App"
+  })
 
   return (
     <>
-      <div className="mb-5">
-        <ExpenseForm addExpense={handleSubmit}/>
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter onSelectCategory={filterExpenses}/>
-      </div>
-      <ExpenseList expenses={expensesFiltered} onDelete={deleteExepense}/>
+      <input ref={ref}/>
     </>
   );
 }
